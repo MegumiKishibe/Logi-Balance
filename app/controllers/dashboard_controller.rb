31b5bbc410dac898378790feb5b4824148a4b4a6
Megumi Kishibe <Kishibe0417@gmail.com>
2025-11-20 19:37:15 +0900

    create Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,19 +6,22 @@ class DashboardController < ApplicationController
 
   # ALL（日別集計）
   def index
-    @course = Course.find(params[:id])
-    @deliveries = @course.deliveries.order(service_date: :desc)
+    @course = Course.find(params[:id]) # コース取得
+    @deliveries = @course.deliveries.order(service_date: :desc) # 配達実績取得
   end
 
   # 単日実績
   def daily
     @course = Course.find(params[:id])
-    @date = params[:date]
 
-    @delivery = @course.deliveries.find_by(service_date: @date)
+    @dates = @course.deliveries.order(service_date: :desc).pluck(:service_date) # 利用可能な日付一覧取得
+
+    @date = params[:date] || @dates.first # パラメータの日付、または最新日付を設定
+
+    @delivery = @course.deliveries.find_by(service_date: @date) # 当日の配達実績取得
 
     if @delivery
-      @stops = @delivery.delivery_stops.order(:completed_at)
+      @stops = @delivery.delivery_stops.where.not(completed_at: nil).order(:completed_at)
     else
       @stops = []
     end
