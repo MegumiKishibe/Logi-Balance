b557336580158705d5e98071cfc0f91378f452eb
Megumi Kishibe <Kishibe0417@gmail.com>
2025-12-20 19:52:11 +0900

    Add Devise columns to employees and make DB config safer locally

--- db/migrate/20251217161654_add_devise_to_employees.rb
+++ db/migrate/20251217161654_add_devise_to_employees.rb
@@ -1,18 +1,26 @@
 # frozen_string_literal: true
 
 class AddDeviseToEmployees < ActiveRecord::Migration[8.0]
-  def self.up
-    change_table :employees do |t|
+  def change
+    change_table :employees, bulk: true do |t|
+      # Devise必須
+      t.string :encrypted_password, null: false, default: ""
+
       # Recoverable
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       # Rememberable
-      # t.datetime :remember_created_at
-    end
-  end
+      t.datetime :remember_created_at
 
-  def self.down
-    raise ActiveRecord::IrreversibleMigration
+      # Trackable（任意だがあると便利）
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+    end
+
+    add_index :employees, :reset_password_token, unique: true
   end
 end
