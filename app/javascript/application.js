05441c6908283070d38d7a740a9dfc1f95298f6a
Megumi Kishibe <Kishibe0417@gmail.com>
2026-01-03 21:14:59 +0900

    fixed errors suggested by rubocop

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,4 @@
-import Rails from "@rails/ujs"
-Rails.start()
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "common/sidebar"
 import "delivery_stops"
-import "chartkick"
-import "Chart.bundle"
-
